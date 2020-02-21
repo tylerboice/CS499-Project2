e6339f7b14b2f86090543069e95d313b3c84ecea
Thatoneguyouknow <benjistaman.12@gmail.com>
2020-02-20 20:12:01 -0700

    Add files via upload

--- KFoldCross.R
+++ KFoldCross.R
@@ -7,11 +7,14 @@ KFoldCV <- function(X_mat, y_vec, ComputePredictions, fold_vec)
 	for (k in 1:K)
 	{
 		# X/Y_new should be all observations related to fold_vec[fold_vec=k]
-		X_new <- X_mat[fold_vec=k]
-		Y_new <- y_vec[fold_vec=k]
+		items <- which(fold_vec == k)
+		notItems <- which(fold_vec != k)
+		# items is a vector containing indices of places where fold_vec = k
+		X_new <- X_mat[items]
+		Y_new <- y_vec[items]
 		# X/Y_train should be all observations not in X/Y_new
-		X_train <- X_mat[!X_new]
-		Y_train <- y_vec[!Y_new]
+		X_train <- X_mat[notItems]
+		Y_train <- y_vec[notItems]
 		pred_new <- ComputePredictions(X_train, Y_train, X_new)
 		errorVec <- ZeroOneLoss(pred_new, Y_new)
 	}
