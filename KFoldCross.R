30096b70f93310947094156e011158a13cb81f92
Tyler Boice <tylerboice@users.noreply.github.com>
2020-02-21 18:03:40 -0700

    Added plotting in prep for NN algorithm

--- KFoldCross.R
+++ KFoldCross.R
@@ -19,60 +19,36 @@ X.raw <- as.matrix(spam.dt[, -ncol(spam.dt), with=FALSE])
 y.vec <- spam.dt[[ncol(spam.dt)]]
 X.sc <- scale(X.raw) # scaled X/feature/input matrix
 
-# # COMMENT BLOCK
-# # compute and visualize validation error as a function of number of neighbors
-# result <- NearestNeighborsCV(X.sc, y.vec)
-#  ggplot()+
-#    geom_line(aes(
-#      neighbors, error.percent, group=validation.fold),
-#      data=validation.error)
-# 
-# ## compute and visualize test error results:
-# err.dt.list <- list()
-# ## assign folds.
-# for(test.fold in 1:5){
-#   ## split into train/test sets.
-#   for(algorithm in c("baseline", "1-NN", "NNCV")){
-#     ## run algorithm and store test error.
-#     err.dt.list[[paste(test.fold, algorithm)]] <- data.table(
-#       test.fold, algorithm, error.percent)
-#   }
-# }
-# err.dt <- do.call(rbind, err.dt.list)
-# 
-# ggplot()+
-#   geom_point(aes(
-#     error.percent, algorithm),
-#     data=err.dt)
 
-# # TEST PLOTTING CODE
+# compute and visualize validation error as a function of number of neighbors
+result <- NearestNeighborsCV(X.sc, y.vec)
+ ggplot()+
+   geom_line(aes(
+     neighbors, error.percent, group=validation.fold),
+     data=validation.error)
+
+# compute and visualize test error results
+err.dt.list <- list()
+# assign folds.
+for(test.fold in 1:5){
+  # split into train/test sets.
+  for(algorithm in c("baseline", "1-NN", "NNCV")){
+    # run algorithm and store test error.
+    err.dt.list[[paste(test.fold, algorithm)]] <- data.table(
+      test.fold, algorithm, error.percent)
+  }
+}
+err.dt <- do.call(rbind, err.dt.list)
+
 ggplot()+
-  geom_line(aes(
-    neighbors, percent.error, color=set, group=paste(set, validation.fold)),
+  geom_point(aes(
+    error.percent, algorithm),
     data=err.dt)
 
-mean.dt <- err.dt[, .(
-  mean.percent=mean(percent.error),
-  sd=sd(percent.error)
-), by=.(set, neighbors)]
-min.dt <- mean.dt[set=="validation"][which.min(mean.percent)]
-gg <- ggplot()+
-  geom_ribbon(aes(
-    neighbors, ymin=mean.percent-sd, ymax=mean.percent+sd, fill=set),
-    alpha=0.5,
-    data=mean.dt)+
-  geom_line(aes(
-    neighbors, mean.percent, color=set),
-    data=mean.dt)+
-  geom_point(aes(
-    neighbors, mean.percent, color=set),
-    data=min.dt)+
-  coord_cartesian(xlim=c(0, 25))
-directlabels::direct.label(gg, "last.polygons")
-# # END TEST PLOTTING CODE
-
+# Compute Predictions is the wrapper for the standard knn function
 ComputePredictions <- function(X_train, y_train,X_new) class::knn(X_train,X_new,y_train)
 
+# K Fold Cross-Validation Algorithm
 KFoldCV <- function(X_mat, y_vec, ComputePredictions, fold_vec)
 {
 	# currently K is a placeholder for testing
